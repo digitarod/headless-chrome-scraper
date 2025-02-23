4f491206fe017054ffe0d2c63c97ce8176145444
digitarod <digitarod@gmail.com>
2025-02-23 23:58:56 +0900

    Update main.py

--- selenium/main.py
+++ selenium/main.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 options = Options()
 options.add_argument("--headless")  # ヘッドレスモード
