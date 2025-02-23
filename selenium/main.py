cb480b6324ce2f69b45dc61f3e03b97cb5709a42
digitarod <digitarod@gmail.com>
2025-02-23 23:47:41 +0900

    Update main.py

--- selenium/main.py
+++ selenium/main.py
@@ -1,12 +1,17 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
 options = Options()
 options.add_argument("--headless")  # ヘッドレスモード
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
-driver = webdriver.Chrome(options=options)
+# `Service` を使用し、`webdriver-manager` で適切な chromedriver を取得
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=options)
+
 driver.get("https://www.google.com")
 print(driver.title)
 
